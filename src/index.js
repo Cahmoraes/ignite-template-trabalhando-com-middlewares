ceb1bc6d577c8d033aa74205827c4a17a2f5eb37
caique.moraes <caique.moraes@compasso.com.br>
2022-06-11 22:24:36 -0300

    envio dos arquivos

--- src/index.js
+++ src/index.js
@@ -10,19 +10,65 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' })
+  }
+
+  request.user = user
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const user = request.user
+
+  if (user.pro) {
+    return next()
+  } else if (user.todos.length < 10) {
+    return next()
+  }
+
+  return response.status(403)
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  if (!validate(id)) {
+    return response.status(400)
+  }
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404)
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if (!todo) {
+    return response.status(404)
+  }
+
+  request.todo = todo
+  request.user = user
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404)
+  }
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
